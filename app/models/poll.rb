45bfbcf91aa4d7caa79687a42288d04b35695390
J Resilient <j.resilient.1@gmail.com>
2020-02-04 16:21:35 -0500

    Add validation to Poll

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,8 @@
 class Poll < ApplicationRecord
+    validates :author_id, presence: true
+    validates :title, presence: true
+    validates :title, uniqueness: { scope: :author_id }
+
     belongs_to :author,
         class_name: 'User',
         foreign_key: :author_id,
