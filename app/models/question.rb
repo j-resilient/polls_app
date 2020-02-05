ed7e6ed0ccdb45ad61878d9c873f574d4e38c177
J Resilient <j.resilient.1@gmail.com>
2020-02-05 15:01:33 -0500

    Add #results

--- app/models/question.rb
+++ app/models/question.rb
@@ -28,5 +28,27 @@ class Question < ApplicationRecord
         foreign_key: :poll_id,
         primary_key: :id
 
-    has_many :responses, through: :answer_choices, source: :responses
+    has_many :responses, 
+        through: :answer_choices, 
+        source: :responses
+
+    def results
+        # AnswerChoice.find_by_sql(<<-SQL)
+        #     SELECT answer_choices.*, COUNT(responses.id) AS response_count
+        #     FROM answer_choices
+        #     LEFT JOIN responses ON answer_choices.id = responses.answer_choice_id
+        #     WHERE answer_choices.question_id = #{self.id}
+        #     GROUP BY answer_choices.id
+        # SQL
+        choices = AnswerChoice
+            .select('answer_choices.*, COUNT(responses.id) AS response_count')
+            .left_outer_joins(:responses)
+            .where('answer_choices.question_id = ?', self.id)
+            .group('answer_choices.id')
+        results = {}
+        choices.each do |answer|
+            results[answer.text] = answer.response_count
+        end
+        results
+    end
 end
